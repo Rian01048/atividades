d5b74dc2249643149aad15d2780b1035f9ac541e
AlanaRaissa11 <alanaraissasantanaaa@gmail.com>
2026-07-22 18:53:19 -0300

    mostrar animais 21/07/2026

--- atividade-4/script.js
+++ atividade-4/script.js
@@ -3,19 +3,19 @@ function mostrarAnimal(value)
     const animal = document.getElementById("animal");
     
     if (value == "gato") {
-        animal.src='gato.jpg'
+        animal.src='assets/gato.jpg'
     }
 
     else if (value == "cachorro") {
-        animal.src='cachorro.jpg'
+        animal.src='assets/cachorro.jpg'
     }
 
     else if (value == "leao") {
-        animal.src='leao.jpg'
+        animal.src='assets/leao.jpg'
     }
 
     else if (value == "elefante") {
-        animal.src='elefante.jpg'
+        animal.src='assets/elefante.jpg'
     }
 
 
