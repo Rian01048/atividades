b0374e4c4d79160695a56188213d9f8381985214
RianSouza_2006 <riansouzasilva247@gmail.com>
2026-08-18 00:28:15 -0300

    Add files via upload

---
+++ Atividade/C#/atividade2/atividade2/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade2
+{
+     class Produto
+    {
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public void ExibirDados(string Nome, float Preco, int Quantidade)
+        {
+            Console.WriteLine($"Nome: {Nome}\nPreço: R${Preco}\nQuantidade: {Quantidade}");
+        }
+    }
+}
