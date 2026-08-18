6172a47608fc7360623e205188d2b66070c735b0
RianSouza_2006 <riansouzasilva247@gmail.com>
2026-08-18 00:29:37 -0300

    Add files via upload

---
+++ Atividade/C#/Atividade/C#/atividade2/atividade2/Program.cs
@@ -0,0 +1,17 @@
+﻿using atividade2;
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite o nome do produto: ");
+        string Nome = Console.ReadLine();
+
+        Console.Write("Digite o preço: ");
+        float preco = float.Parse(Console.ReadLine());
+
+        Console.Write("Digite a quantidade: ");
+        int quant = int.Parse(Console.ReadLine());
+        new Produto().ExibirDados(Nome, preco, quant);
+    }
+}
---
+++ Atividade/C#/Atividade/C#/atividade3/atividade3/ContaBancaria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade3
+{
+    class ContaBancaria
+    {
+        public string Titular { get; set; }
+        public float Saldo { get; set; }
+
+        public void Depositar(float valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(float valor)
+        {
+            Saldo -= valor;
+        }
+
+    }
+}
